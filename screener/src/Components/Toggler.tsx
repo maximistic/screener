b4a2e8a90ad22a36c7d4b2cb518bc939e09201b7
Sri <110153672+maximistic@users.noreply.github.com>
2024-09-09 12:06:43 +0530

    Create Toggler.tsx

---
+++ screener/src/Components/Toggler.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import React, { useState } from 'react';
+import SigninForm from '@/Components/SigninForm';
+import SignupForm from '@/Components/SignupForm';
+
+const Toggler = () => {
+    const [showSignin, setShowSignin] = useState(false);
+
+    const toggleForm = () => {
+      setShowSignin(prev => !prev);
+    };
+  
+    return (
+      <main className="h-screen overflow-hidden">
+        <div 
+          className={`transition-transform duration-1000 ease-in-out transform ${showSignin ? '-translate-y-1/2' : 'translate-y-0'}`}
+          style={{ height: '200%' }}
+        >
+          <div className="h-1/2">
+            <SignupForm onSignInClick={toggleForm} />
+          </div>
+          <div className="h-1/2">
+            <SigninForm onSignUpClick={toggleForm} />
+          </div>
+         </div>
+      </main>
+    );
+}
+
+export default Toggler
